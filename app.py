31b2e6995cd057391d391ddd83e61441906d7602
Yash jaiswal <callmeyash800@gmail.com>
2026-03-31 17:55:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 # ---------------------------
 # API URL (FastAPI)
 # ---------------------------
-API_URL = "http://127.0.0.1:8000"
+API_URL = "http://fastapi:8000"
 
 # ---------------------------
 # Sidebar
